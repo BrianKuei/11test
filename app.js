de7f302d18f40a66eb9844b988ececc44aa02aa2
Kuei <vx3012564897@gmail.com>
2019-11-11 19:29:06 +0800

    add js

---
+++ app.js
@@ -0,0 +1,42 @@
+// var count = 0;
+// global.setInterval(function() { console.log(`hello ${count++}`); },1000);
+
+
+
+var http = require("http");
+//  node.js 載入模組
+
+http.createServer(function(request, response) {
+  console.log(request.url);
+  if(request.url == '/'){
+// 路由
+    // 資料庫
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>index</h1>");
+    response.end();
+  }else if(request.url == '/search'){
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>search</h1>");
+    response.end();
+  }else if(request.url == '/error'){
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>error</h1>");
+    response.end();
+  }else if(request.url == '/shop'){
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>shop</h1>");
+    response.end();
+  }else if(request.url == '/checkout'){
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>checkout</h1>");
+    response.end();
+  }
+  
+}).listen(process.env.PORT ||3000);
+console.log("Server已開啟port: 3000.");
+
